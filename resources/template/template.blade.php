cfb9a281826763340e33bac6eae731d41525c0be
Maenda Fortier <maenda@pixelunion.net>
2017-11-28 09:10:40 -0800

    Preliminary post fetching

--- resources/template/template.blade.php
+++ resources/template/template.blade.php
@@ -1,3 +1,21 @@
-<section>
-    This is where the template will go!
+<script src="https://unpkg.com/jquery@3.2.1/dist/jquery.js"></script>
+<script src="https://s3-us-west-2.amazonaws.com/bc-theme-utils/twitter-widget/scripts/widget.js"></script>
+<section class="tweets">
+    @for ($i=0; $i<4; $i++)
+        <article class="tweet">
+            <a
+                href="#"
+                class="tweet--header"
+                target="__blank"
+            >
+                <span class="tweet--header-avatar"></span>
+                <span class="tweet--header-name">Your Twitter Account</span>
+                <span class="tweet--header-screenname">@youraccount</span>
+            </a>
+            <span class="tweet--content"></span>
+            <span class="tweet--footer">
+                <span class="tweet--footer--posted"></span>
+            </span>
+        </article>
+    @endfor
 </section>
