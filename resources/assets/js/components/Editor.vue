996c60c9c8d13aa6431d7202d1fd74c987ba6d3c
Maenda Fortier <maenda@pixelunion.net>
2017-12-01 15:02:20 -0800

    More precise styling and notifications

--- resources/assets/js/components/Editor.vue
+++ resources/assets/js/components/Editor.vue
@@ -1,95 +1,132 @@
 <template>
-    <div class="container">
-        <input
-            name="store-hash"
-            type="hidden"
-             v-model="storeHash"
-        />
-        <div class="field">
-            <label for="handle" class="label">Twitter handle</label>
-            <div class="control">
-                <input
-                    name="handle"
-                    class="input"
-                    v-model="handle"
-                    placeholder="@username"
-                />
-            </div>
-            <p class="help">
-                Please go ahead and enter your Twitter handle.
-            </p>
-        </div>
-        <div class="field">
-            <label for="number-posts" class="label">Number of Posts</label>
-            <div class="control">
-                <div class="select is-fullwidth">
-                    <select name="number-posts" v-model="howMany">
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                        <option value="4">Four</option>
-                    </select>
-                </div>
-            </div>
-            <p class="help">
-                This allows you to choose the amount of Twitter posts to display to your customers
-            </p>
-        </div>
-
-        <div class="field">
-            <label for="theme" class="label">Theme</label>
-            <div class="control">
-                <div class="select is-fullwidth">
-                    <select v-model="widgetStyle">
-                        <option value="light">Light</option>
-                        <option value="dark">Dark</option>
-                        <option value="alternate">Alternate</option>
-                    </select>
-                </div>
-            </div>
-            <p class="help">
-                Choose a particular alternative version of the twitter theme.
-            </p>
-        </div>
-        <div class="control">
-            <button v-on:click="saveWidget" class="button">Save</button>
-        </div>
+  <div>
+    <div class="merchant-branding">
+      <img
+        src="https://s3-us-west-2.amazonaws.com/bc-theme-utils/twitter-widget/img/merchant_logo.png"
+        alt="Merchant Logo"
+      />
     </div>
+    <div v-if="success" class="notification is-primary">
+      <button class="delete" @click="closeNotifications"></button>
+      Successfully saved!
+    </div>
+    <div v-if="error" class="notification is-danger">
+      <button class="delete" @click="closeNotifications"></button>
+      Sorry there was an error
+    </div>
+    <div class="container">
+      <h1 class="is-title">
+        Your Merchant Twitter Feed
+      </h1>
+    </div>
+    <div class="container">
+      <input
+          name="store-hash"
+          type="hidden"
+            v-model="storeHash"
+      />
+      <div class="field">
+        <label for="handle" class="label">Twitter handle</label>
+        <div class="control">
+          <input
+            name="handle"
+            class="input"
+            v-model="handle"
+            placeholder="@username"
+           />
+        </div>
+        <p class="help">
+          Please go ahead and enter your Twitter handle.
+        </p>
+      </div>
+      <div class="field">
+        <label for="number-posts" class="label">Number of Posts</label>
+        <div class="control">
+          <div class="select is-fullwidth">
+            <select name="number-posts" v-model="howMany">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+            </select>
+          </div>
+        </div>
+        <p class="help">
+          This allows you to choose the amount of Twitter posts to display to your customers
+        </p>
+      </div>
+      <div class="field">
+        <label for="theme" class="label">Theme</label>
+        <div class="control">
+          <div class="select is-fullwidth">
+            <select v-model="widgetStyle">
+                <option value="light">Light</option>
+                <option value="dark">Dark</option>
+                <option value="alternate">Alternate</option>
+            </select>
+          </div>
+        </div>
+        <p class="help">
+            Choose a particular alternative version of the twitter theme.
+        </p>
+      </div>
+      <div class="control">
+          <button
+          @click="saveWidget"
+          :class="{ 'button' : true,  'is-loading' : submitInProgress }"
+          :disabled='submitInProgress'
+        >
+          Save
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-  import axios from 'axios';
+import axios from "axios";
 
-  export default {
-    props: [
-      'twitterHandle',
-      'context',
-      'numPosts',
-      'colorStyle'
-    ],
-    data: function () {
-      return {
-        handle: this.twitterHandle,
-        storeHash: this.context,
-        howMany: this.numPosts,
-        widgetStyle: this.colorStyle
-      }
-    },
-    methods: {
-      saveWidget: function() {
-        axios.post('/save', {
+export default {
+  props: ['twitterHandle', 'context', 'numPosts', 'colorStyle'],
+  data: function() {
+    return {
+      handle: this.twitterHandle,
+      storeHash: this.context,
+      howMany: this.numPosts,
+      widgetStyle: this.colorStyle,
+      submitInProgress: false,
+      success: false,
+      error: false
+    };
+  },
+  methods: {
+    saveWidget: function() {
+      this.submitInProgress = true;
+      this.success = false;
+      this.error = false;
+      axios
+        .post("/save", {
           twitterHandle: this.handle,
           context: this.storeHash,
           numberPosts: this.howMany,
           style: this.widgetStyle
-        }).then(function(response){
-          if (response.status === 200) {
-            console.log(response);
-          } else {
-            console.log('There was an error saving your configuration');
-          }
         })
-      }
+        .then(
+          function(response) {
+            this.submitInProgress = false;
+            this.success = true;
+            window.scrollTo(0, 0);
+        }.bind(this))
+        .catch(function(error) {
+          this.submitInProgress = false;
+          this.error = true;
+          window.scrollTo(0, 0);
+        }.bind(this));
+    },
+    closeNotifications: function () {
+      this.success = false;
+      this.error = false;
     }
   }
+};
 </script>
--- resources/views/editor.blade.php
+++ resources/views/editor.blade.php
@@ -5,33 +5,23 @@
     <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}" />
   </head>
   <body>
-    <div class="merchant-branding">
-      <img
-        src="https://s3-us-west-2.amazonaws.com/bc-theme-utils/twitter-widget/img/merchant_logo.png"
-        alt="Merchant Logo"
-      />
-    </div>
-    <div class="container">
-      <h1 class="is-title">
-        Your Merchant Twitter Feed
-      </h1>
-    </div>
-    <div id="widget-editor">
-      <editor
-        :twitter-handle="'{{$user['twitter_handle']}}'"
-        :context="'{{$user['context']}}'"
-        :num-posts="'{{$user['number_posts']}}'"
-        :color-style="'{{$user['style']}}'"
-      >
-      </editor>
-    </div>
-    <div class="container">
-      <div class="level">
-        <div class="level-item">
-          @include('pxu-branding')
+    <section id="widget-editor" class="main-section">
+        <editor
+          :twitter-handle="'{{$user['twitter_handle']}}'"
+          :context="'{{$user['context']}}'"
+          :num-posts="'{{$user['number_posts']}}'"
+          :color-style="'{{$user['style']}}'"
+        >
+        </editor>
+      </div>
+      <div class="container pxu-branding-container">
+        <div class="level">
+          <div class="level-item">
+            @include('pxu-branding')
+          </div>
         </div>
       </div>
-    </div>
+    </section>
     <script src="{{ mix('js/app.js') }}"></script>
   </body>
 </html>
