5e05619bde69eb6468468f4b94f0fe0d713bd8c5
Maenda Fortier <maenda@pixelunion.net>
2017-12-22 13:48:05 -0800

    Make separate vue components work together properly

--- resources/assets/js/components/Notifications.vue
+++ resources/assets/js/components/Notifications.vue
@@ -1,12 +1,10 @@
 <template>
   <div>
     <div v-if="success" class="notification is-primary">
-      <button class="delete" @click="closeNotifications" />
-      Successfully saved!
+      The changes you've made to your Twitter widget have been saved.
     </div>
     <div v-if="error" class="notification is-danger">
-      <button class="delete" @click="closeNotifications" />
-      Sorry there was an error
+      Sorry there was an error. Please try again and contact us if the problem persists" is all you might do.
     </div>
   </div>
 </template>
