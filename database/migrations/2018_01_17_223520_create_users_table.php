acf1106a251958cf98307bb99c76a9f1ca8c2b07
Maenda Fortier <maenda@pixelunion.net>
2018-01-18 10:18:06 -0800

    Add users table migration

---
+++ database/migrations/2018_01_17_223520_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id')->unique();
+            $table->string('access_token');
+            $table->string('context');
+            $table->integer('user_id');
+            $table->string('twitter_handle');
+            $table->integer('number_posts')->default(4);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
